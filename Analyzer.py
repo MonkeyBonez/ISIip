82fc34f5487758d69c0a2ae6c6357b88d1e92f8b
Harshul Mulchandani <vibrane@gmail.com>
2017-06-14 11:53:13 -0700

    fixed frequency

--- Analyzer.py
+++ Analyzer.py
@@ -5,12 +5,14 @@ inputfile = open("list.txt", "r")
 countryFrequency = {}
 countryRequestFrequency = {}
 asnFrequency = {}
+count = 0
 next(inputfile)
 for line in inputfile:
     ip = line.split()[0]
     country = line.split()[1]
     asn = line.split()[2]
     requests = int(line.split()[3])
+    count += 1
 
     if country not in countryFrequency:
         countryFrequency[country] = 0
@@ -31,7 +33,7 @@ asnFrequency = OrderedDict(sorted(asnFrequency.items(), key=itemgetter(1)))
 
 print "\n\t% Country IP in top 1000"
 for country in countryFrequency:
-    print country + " " , float(countryFrequency[country])/1000
+    print country + " " , float(countryFrequency[country])/count
 
 print "\n\t% Top country requests in top 1000"
 for country in countryRequestFrequency:
@@ -39,4 +41,4 @@ for country in countryRequestFrequency:
 
 print "\n\t% Top ASN in top 1000"
 for asn in asnFrequency:
-    print asn + " " , float(asnFrequency[asn])/1000
+    print asn + " " , float(asnFrequency[asn])/count

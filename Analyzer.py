39819d7c2bf5388f76762ef6bb35012e17ec0e09
Harshul Mulchandani <vibrane@gmail.com>
2017-06-14 12:50:45 -0700

    asn requests

--- Analyzer.py
+++ Analyzer.py
@@ -5,6 +5,7 @@ inputfile = open("list.txt", "r")
 countryFrequency = {}
 countryRequestFrequency = {}
 asnFrequency = {}
+asnRequestFrequency={}
 count = 0
 next(inputfile)
 for line in inputfile:
@@ -26,19 +27,28 @@ for line in inputfile:
         asnFrequency[asn] = 0
     asnFrequency[asn] += 1
 
+    if asn not in asnRequestFrequency:
+        asnRequestFrequency[asn] = requests
+    asnRequestFrequency[asn] += requests
+
 
 countryFrequency = OrderedDict(sorted(countryFrequency.items(), key=itemgetter(1)))
 countryRequestFrequency = OrderedDict(sorted(countryRequestFrequency.items(), key=itemgetter(1)))
 asnFrequency = OrderedDict(sorted(asnFrequency.items(), key=itemgetter(1)))
+asnRequestFrequency = OrderedDict(sorted(asnRequestFrequency.items(), key=itemgetter(1)))
 
-print "\n\t% Country IP in top 1000"
+print "\n\t% Country IP in top 10000"
 for country in countryFrequency:
     print country + " " , float(countryFrequency[country])/count
 
-print "\n\t% Top country requests in top 1000"
+print "\n\t% Top country requests in top 10000"
 for country in countryRequestFrequency:
     print country + " " , float(countryRequestFrequency[country])/sum(countryRequestFrequency.values())
 
-print "\n\t% Top ASN in top 1000"
+print "\n\t% Top ASN in top 10000"
 for asn in asnFrequency:
     print asn + " " , float(asnFrequency[asn])/count
+
+print "\n\t% Top ASN requests in top 10000"
+for asn in asnRequestFrequency:
+    print asn + " " , float(asnRequestFrequency[asn])/sum(asnRequestFrequency.values())

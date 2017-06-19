414bfd68ba889f0dd553ed69426c8fe6a94af612
watereaper <mulchandanisnehal1@gmail.com>
2017-06-19 00:28:54 -0700

    Goes through list of ips and checks if port 53 is open for connection.

--- PortConnector.py
+++ PortConnector.py
@@ -1,12 +1,26 @@
 import socket
 import re
 import sys
+inputfile = open("reqs.out", "r")
+count = 0
+DNS_PORT = 53
 s = socket.socket()
-address = '136.243.94.106'
-port = 53
-print ("Attempting to connect to %s on port %s" % (address, port))
-try:
-    s.connect((address, port))
-    print ("Connected to %s on port %s" % (address, port))
-except socket.error:
+
+def ipPortConnector (address, port):
+    print ("Attempting to connect to %s on port %s" % (address, port))
+    try:
+        s.connect((address, port))
+        print ("Connected to %s on port %s" % (address, port))
+    except socket.error:
         print ("Connection to %s on port %s failed" % (address, port))
+
+for line in inputfile:
+    count+= 1
+    ip = line.split()[0]
+    ipPortConnector(ip,DNS_PORT)
+    if count > 20:
+        break
+
+
+
+
